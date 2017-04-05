594669517462cc66dcdef4782e3d883f8f524b82
Ruslan <mirhusainow@gmail.com>
2017-04-05 13:59:41 +0300

    MinusStopWords

--- src/java/LSA.java
+++ src/java/LSA.java
@@ -1,41 +1,56 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Ruslan on 10.03.2017.
  */
 public class LSA {
-    private ArrayList<String> dictionary = new ArrayList<>();
-    private ArrayList<String> stopWords = new ArrayList<>();
-//    private String[] symbols = {",",".",";",":","-","'","!","?"};
+    private ArrayList<String> dictionary;
+    private ArrayList<String> stopWords;
     private static final String SYMBOLS = "[^а-яА-Яa-zA-Z\\s]";
     private Stemmer stemmer;
 
-    public LSA() {
+    public LSA(ArrayList stopWords) {
         stemmer = new Stemmer();
+        this.stopWords =stopWords;
     }
 
     public void doLSA(ArrayList<String> documents){
+
         preProcessing(documents);
     }
 
     private ArrayList preProcessing(ArrayList<String> documents){
-        ArrayList<String[]> result = new ArrayList<>();
+        ArrayList<List> result = new ArrayList<>();
 
         for (String doc:documents) {
             doc = doc.replaceAll(SYMBOLS,"");
-            String[] words = doc.split("\\s");
+            doc = doc.toLowerCase();
 
-            for (int i = 0; i < words.length; i++){
-                words[i] = stemmer.stem(words[i]);
+            ArrayList<String> words = new ArrayList<String>(Arrays.asList(doc.split("\\s")));
+            Iterator<String> iterator = words.iterator();
+
+            while (iterator.hasNext()){
+                String word = iterator.next();
+                if (stopWords.contains(word)){
+                    iterator.remove();
+                }
+                word = stemmer.stem(word);
             }
             result.add(words);
         }
 
-        for (String[] words:result) {
+        for (List<String> words:result) {
             for (String word:words){
                 System.out.println(word);
             }
         }
         return result;
+    }
+
+    private void doTheMatrix(ArrayList<List> data){
+
     }
 }
--- src/java/MainTest.java
+++ src/java/MainTest.java
@@ -4,11 +4,21 @@ import java.util.ArrayList;
  * Created by Ruslan on 10.03.2017.
  */
 public class MainTest {
+    private static ArrayList<String> stopWords;
     public static void main(String[] args) {
-        LSA lsa = new LSA();
+
+        stopWords = new TxtReader().loadFile("src/res/stop-words.txt");
+        if (stopWords == null)
+            System.out.println("null");
+
+        System.out.println(stopWords);
+        LSA lsa = new LSA(stopWords);
+
+
         ArrayList<String> test = new ArrayList<>();
         test.add("Британская полиция знает о местонахождении основателя WikiLeaks");
         test.add("В суде США начинается процесс против россиянина, рассылавшего спам");
+
         lsa.doLSA(test);
     }
 }
